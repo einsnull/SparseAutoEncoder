6502cc4ab3c02508f32ab78f47b22c728088b52c
Qingbiao Miao <einsn56@gmail.com>
2014-02-12 15:50:00 +0800

    Debug

--- SparseAutoEncoder/testSAE.cpp
+++ SparseAutoEncoder/testSAE.cpp
@@ -12,8 +12,8 @@ using namespace std;
 
 bool loadDataSet(MatrixXd &data);
 
-void toImage(MatrixXd theta,int imgWidth,
-			 char *szFileName,bool showFlag = false);
+void toImage(MatrixXd &theta,int imgWidth,
+			 char *szFileName,bool showFlag = false,double ratio = 1);
 
 int main()
 {
@@ -69,7 +69,8 @@ int main()
 		return -1;
 	}
 	//save the top 100 examples to image
-	toImage(trainingData.topRows(100),imgWidth,"data.bmp");
+	MatrixXd showImage = trainingData.topRows(100);
+	toImage(showImage,imgWidth,"data.bmp");
 	clock_t start = clock();
 	//train the sparse auto encoder
 	sae.train(trainingData,lambda,alpha,beta,sp,
@@ -114,58 +115,82 @@ bool loadDataSet(MatrixXd &data)
 	return true;
 }
 
-void toImage(MatrixXd theta,int imgWidth,
-			 char* szFileName,bool showFlag)
+void toImage(MatrixXd &theta,int imgWidth,char* szFileName,bool showFlag,double ratio)
 {
-	int ratio = 10;
 	int margin = 1;
 	int rows = theta.rows();
 	int cols = theta.cols();
-	int perRow = (int)(sqrt(rows) + 0.5);
-	//calculate the total cols 
-	int tCols = (int)(rows / (double)perRow + 0.5);
-	//get the max values of each row
+	if(rows <= 0 || cols <= 0)
+	{
+		return ;
+	}
+	/*cout << rows << endl;
+	cout << cols << endl;*/
+	double pr = sqrt((double)rows);
+	int perRow = (int)pr + (pr - (int)pr > 0);
+	double tc = (double)rows / (double)perRow;
+	int tCols = (int)tc + (tc - (int)tc > 0);
+	/*cout << "perRow: " << perRow << endl;
+	cout << "tCols: " << tCols << endl;*/
 	MatrixXd max = theta.rowwise().maxCoeff();
-	//get the min values of eache row
 	MatrixXd min = theta.rowwise().minCoeff();
 
 	int imgHeight = cols/imgWidth;
 	IplImage* iplImage = cvCreateImage(
-		cvSize(imgWidth * perRow + margin * (perRow+1),imgHeight * tCols
-		+ margin * (tCols + 1)),
+		cvSize(imgWidth * perRow + margin * (perRow+1),imgHeight * tCols + margin * (tCols + 1)),
 		IPL_DEPTH_8U,1);
+	
 	int step = iplImage->widthStep;
 	uchar *data = (uchar *)iplImage->imageData;
+	int h = iplImage->height;
+	int w = iplImage->width;
+	for(int x = 0; x < w; x++)
+	{
+		for(int y = 0; y < h; y++)
+		{
+			data[y * step + x] = 0;
+		}
+	}
 
 	for(int i = 0;i < rows;i++)
 	{
+		
 		int n = 0;
+		int hIdx = i / perRow;
+		int wIdx = i % perRow;
+
 		for(int j = 0;j < imgHeight;j++)
 		{
 			for(int k = 0;k < imgWidth; k++)
 			{
-				double per = (theta(i,n) - min(i,0) )/ (max(i,0) - min(i,0));
-			
-				data[(j + margin + (i / perRow * (imgHeight + margin))) * step
-					+ margin + k + (i % perRow)*imgWidth + (i % perRow) * margin] = 
-					(uchar)(int)(per * 255.0);
+				double per = (theta(i,n) - min(i,0) ) / (max(i,0) - min(i,0));
+				//data[j * step + k] = 255;
+				int val = (hIdx * imgHeight+j + margin * (hIdx+1)) * step
+					+ (wIdx * imgWidth + k + margin * (wIdx+1));
+				if(val > step * (imgHeight * tCols + margin * (tCols + 1)))
+				{
+					cout << "error" << endl;
+				}
+				data[val] = (uchar)(int)(per * 230.0);
 				n ++;
 			}
 		}
 		
 	}
+	
 	cvSaveImage(szFileName,iplImage);
+
 	if(showFlag)
 	{
 		cvNamedWindow("Image",CV_WINDOW_AUTOSIZE);
-		IplImage* iplImageShow = cvCreateImage(cvSize(
-			iplImage->width * ratio,iplImage->height * ratio),IPL_DEPTH_8U,1);
+		IplImage* iplImageShow = cvCreateImage(
+			cvSize((int)(iplImage->width * ratio),(int)(iplImage->height * ratio)),IPL_DEPTH_8U,1);
 		cvResize(iplImage,iplImageShow,CV_INTER_CUBIC);
 		cvShowImage("Image",iplImageShow);
 		cvWaitKey(100000);
 		cvDestroyWindow("Image");
 		cvReleaseImage(&iplImageShow);
-		cvReleaseImage(&iplImage);
 	}
+	cvReleaseImage(&iplImage);
 }
 
